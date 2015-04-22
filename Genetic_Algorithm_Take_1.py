dd9dafa01159c824fb771960667e682dd8ae40c4
Michael Kilby <kilbyjmichael@gmail.com>
2015-04-22 13:43:43 -0500

    Create GA_Take_1.py

---
+++ Genetic_Algorithm_Take_1.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+import random
+
+TARGET     = "Michael Kilby"
+DNA_SIZE    = len(TARGET)
+POP_SIZE    = 100
+GENERATIONS = 1000
+MUTATE_CHANCE = [1,100] # 1 in 100
+
+def random_char():
+  # Return a random character between ASCII 32 and 126
+  return chr(int(random.randrange(32, 126, 1)))
+  
+def make_population():
+    # Returns a list of POP_SIZE strings.
+    population = ['''list''']
+    for x in range(POP_SIZE):
+        dna = ''
+        for char in range(DNA_SIZE):
+            dna += random_char()
+        population.append(dna)
+    return population
+
+def mutate(dna_sample):
+    # Use MUTATE_CHANCE to create randomness in population
+    dna = ''
+    for char in range(DNA_SIZE):
+        if int(random.random()*(MUTATE_CHANCE[1]/MUTATE_CHANCE[0])) == 1:
+            dna += random_char()
+        else:
+            dna += dna_sample[char]
+    return dna
+
+def fitness(dna):
+    # Calculate the difference between a character in the same position in the TARGET string.
+    fitness = 0
+    for c in range(DNA_SIZE):
+        fitness += abs(ord(dna[c]) - ord(TARGET[c]))
+    return fitness
+  
+def mate(mother, father):
+    zygote = list(zip(mother, father))
+    child = ''
+    for dna in child:
+        if dna[0] == dna[1]:
+            child += dna[0]
+        else:
+            child += dna[random.randrange(2)]
+    return child
+
+def weighted_choice(items):
+    """
+    Chooses a random element from items, where items is a list of tuples in
+    the form (item, weight). weight determines the probability of choosing its
+    respective item. Note: this function is borrowed from ActiveState Recipes.
+    """
+    weight_total = sum((item[1] for item in items))
+    n = random.uniform(0, weight_total)
+    for item, weight in items:
+      if n < weight:
+        return item
+      n = n - weight
+    return item
+
+def main():
+    # Make an initial population
+    population = make_population()
+    
+    for generation in range(GENERATIONS):
+        weighted_population = []
+       
+        for individual in population:
+            fitness_num = fitness(individual)
+        
+        if fitness_num == 0:
+            pair = (individual, 1)
+        else:
+            pair = (individual, fitness_num)
+        
+        weighted_population.append(pair)
+        print("Generation %s... Random sample: '%s'" % (generation, weighted_population[0]))
+        
+        population = [] # Empty the houses for the kids
+    
+    for _ in xrange(POP_SIZE*.2):
+      # Selection
+      person1 = weighted_choice(weighted_population)
+      person2 = weighted_choice(weighted_population)
+ 
+      # Crossover
+      person1, person2 = crossover(person1, person2)
+ 
+      # Mutate and add back into the population.
+      population.append(mutate(person1))
+      population.append(mutate(person2))
+      
+    fittest_string = population[0]
+    minimum_fitness = fitness(population[0])
+ 
+    for individual in population:
+        ind_fitness = fitness(individual)
+    if ind_fitness <= minimum_fitness:
+        fittest_string = individual
+        minimum_fitness = ind_fitness
+    print("Fittest String: %s" % fittest_string)
+
+if __name__ == "__main__": main()
